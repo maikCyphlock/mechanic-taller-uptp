6393ab3a5c1cfdc33a8f9057481abf802c7896ab
maikCyphlock <maikolaguilar656@gmail.com>
2025-06-01 20:05:29 -0400

    fix: Update user type in Locals interface to include schema inference

--- env.d.ts
+++ env.d.ts
@@ -1,10 +1,10 @@
- 
+ import type { user } from '@/db/schema';
 /// <reference path="../.astro/types.d.ts" />
  
 declare namespace App {
     // Note: 'import {} from ""' syntax does not work in .d.ts files.
     interface Locals {
-        user: import("better-auth").User | null;
+        user: (import("better-auth").User & typeof import("src/db/schema").user.$inferSelect) | null;
         session: import("better-auth").Session | null;
     }
 }
