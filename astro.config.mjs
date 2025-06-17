82c77ef6ed01f15516fc9ce2fbadae787f4cd747
maikCyphlock <maikolaguilar656@gmail.com>
2025-06-17 15:03:29 -0400

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import PoweredWebAppBuilder from "webapp-astro-pwa/pwa";
 import svelte from "@astrojs/svelte";
 import node from '@astrojs/node'
 import tailwindcss from "@tailwindcss/vite";
+
 import vercel from "@astrojs/vercel";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,19 +15,19 @@ export default defineConfig({
     integrations: [
         PoweredWebAppBuilder({}), 
         svelte()
-    ],
-    adapter: vercel({
-        edgeMiddleware: true,
-        includeFiles: ['./src/**/*'],
-        maxDuration: 10
-    }),
-    vite: {
-        plugins: [tailwindcss()],
-        ssr: {
-            noExternal: ['@astrojs/vercel']
-        },
-        optimizeDeps: {
-            exclude: ['@astrojs/vercel']
-        }
-    }
+        ],
+    adapter: netlify(),
+    vite:{
+        plugins:[tailwindcss()]
+    },
+
+
+  // vite:{
+  //   server:{
+  //     cors: {
+  //       origin: "maikol.com"
+  //     },
+  //     allowedHosts:["maikol.com"]
+  //   }
+  // }
 });
