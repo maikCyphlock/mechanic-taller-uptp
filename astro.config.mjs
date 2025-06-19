bf421c935567050c84f7998a4ee6ea47d7685008
maikCyphlock <maikolaguilar656@gmail.com>
2025-06-19 18:26:59 -0400

    fix error on render

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
     }),
     server:{
         //@ts-ignore
-        port: process.env.PORT || 4321,
+        port: process.env.port ,
         host: "0.0.0.0"
     },
     vite:{
