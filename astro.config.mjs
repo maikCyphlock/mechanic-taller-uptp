a021ebdf55c0a1e08a983b94910b115e6c177d87
maikCyphlock <maikolaguilar656@gmail.com>
2025-06-19 18:39:06 -0400

    Update server port configuration in astro.config.mjs to use only the PORT environment variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
     }),
     server:{
         //@ts-ignore
-        port: process.env.PORT || 4321,
+        port: process.env.PORT,
         host: "0.0.0.0"
     },
     vite:{
