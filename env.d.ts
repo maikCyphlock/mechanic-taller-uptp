c4f48faa07a456ac6d52c5af4ed3203f7d5cc6cb
maikCyphlock <maikolaguilar656@gmail.com>
2025-06-19 19:53:53 -0400

    fix: error on types

--- env.d.ts
+++ env.d.ts
@@ -1,10 +1,11 @@
- import type { user } from '@/db/schema';
-// <reference path="../.astro/types.d.ts" />
- 
-declare namespace App {
-    // Note: 'import {} from ""' syntax does not work in .d.ts files.
-    interface Locals {
-        user: import("better-auth").User;
-        session: import("better-auth").Session | null;
+/// <reference types="astro/client" />
+import type { user } from '@/db/schema';
+
+declare global {
+    namespace App {
+        interface Locals {
+            user: import("better-auth").User;
+            session: import("better-auth").Session | null;
+        }
     }
 }
